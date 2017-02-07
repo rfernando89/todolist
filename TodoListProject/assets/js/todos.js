51a4e85b58626dc7d7fc9c753074c20700d059b7
Ryan Fernando <rfernando89@csu.fullerton.edu>
2017-02-07 13:51:44 -0800

    Add files via upload

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,37 @@
+// Check off specific todos 
+// When an li is clicked inside of a ul, run this code.
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+// When X is clicked inside of a ul, delete the span
+$("ul").on("click","span",function(event){
+	//span  // li faded out
+	$(this).parent().fadeOut(500,function(){
+		// fade out the li
+		$(this).remove();
+	});
+
+	// stop bubbling
+	event.stopPropagation();
+});
+
+// When the user pushes enter, the input from the user will be added 
+// to the todo list
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13)
+	{
+		// Get input from user
+		var todoText = $(this).val();
+		// Clear the user input text
+		$(this).val("");
+		// create a new li and add to ul
+		
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>").hide().fadeIn(500);
+	}
+});
+
+$(".fa-pencil-square-o").click(function(){
+	$("input[type='text']").fadeToggle();
+});
